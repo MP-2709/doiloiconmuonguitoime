4b9fba0c946dd1480f8e08a98db1bdc68f330f3c
MP-2709 <k62.2311110189@ftu.edu.vn>
2024-10-19 11:47:33 +0700

    đôi lời con muốn gửi tới mẹ iu <3

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Con chào mẹ, con có điều muốn nói. </br> Mẹ mau khỏi bệnh nhé ạ!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
+    "Mẹ ấn vào nút ĐỒNG Ý để xem nhé ạ.",
+  buttonYes: "Mẹ đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Con biết mẹ sẽ đồng ý mà.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Ngày 20.10 năm nay thật đặc biệt khi mẹ đang bị bệnh, cũng là năm đầu tiên em vào cấp 3, bố chuyển công tác và năm thứ 2 con xa nhà. </br> Nhân ngày 20/10 con cũng không biết chúc mẹ gì hơn ngoài dồi dào sức khỏe, luôn hạnh phúc với bố con con và thuận lợi trong công việc cũng như là cuộc sống. </br> Tối nay bố con con sẽ có 1 bữa tiệc nho nhỏ để chúc mừng ngày của Mẹ và sinh nhật em. Con mong mẹ sẽ được đón nhận nó 1 cách trọn vẹn nhất. </br> Love you mom <3",
 };
